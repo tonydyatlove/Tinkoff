b89c1e7e9ebf3abe5d852682dd073e16eb342766
tonydyatlove <106832714+tonydyatlove@users.noreply.github.com>
2023-06-06 13:18:50 +0300

    Add files via upload

--- TrainingTasks/Main.java
+++ TrainingTasks/Main.java
@@ -1,24 +1,26 @@
-/*
-Костя подключен к мобильному оператору «Мобайл». Абонентская плата Кости составляет A рублей в месяц. 
-За эту стоимость Костя получает B мегабайт интернет-трафика. Если Костя выйдет за лимит трафика, то каждый следующий мегабайт будет стоить ему C рублей.
-Костя планирует потратить D мегабайт интернет-трафика в следующий месяц. Помогите ему сосчитать, во сколько рублей ему обойдется интернет.
-*/
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public static int overallCost(int subscriptionFee, int receivedMegabytes, int costOfAdditionalMegabyte, int spentMegabytes) {
-        int cost = subscriptionFee;
-        if (spentMegabytes > receivedMegabytes)
-            cost += (spentMegabytes - receivedMegabytes) * costOfAdditionalMegabyte;
-        return cost;
+    public static int cutTheRoll(int amountOfPeople) {
+        int numOfCuts;
+        double degree = Math.log(amountOfPeople) / Math.log(2);
+        if (amountOfPeople == 1)
+            numOfCuts = 0;
+        else if (degree % 1 == 0)
+            numOfCuts = (int) degree;
+        else
+            numOfCuts = (int) degree + 1;
+
+        return numOfCuts;
     }
 
     public static void main(String[] args) {
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
             String line = bufferedReader.readLine();
-            String[] inputData = line.split("  ");
-            System.out.println(overallCost(Short.parseShort(inputData[0]), Short.parseShort(inputData[1]), Short.parseShort(inputData[2]), Short.parseShort(inputData[3])));
+            System.out.println(cutTheRoll(Integer.parseInt(line)));
         } catch (IOException e) {
             e.printStackTrace();
         }
